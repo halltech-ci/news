d471eb5ea1ac78901b0c5cb177ce10ae0bb38677
sagno <sagnodaouda@gmail.com>
2021-09-17 18:19:19 +0000

    modifier manifeste

--- rapport/__manifest__.py
+++ rapport/__manifest__.py
@@ -10,10 +10,10 @@ The specific and easy-to-use Invoicing system in Odoo allows you to keep track o
 
 You could use this simplified accounting in case you work with an (external) account to keep your books, and you still want to keep track of payments. This module also offers you an easy method of registering payments, without having to encode complete abstracts of account.
     """,
-    'category': 'Uncategorized',,
+    'category': 'Uncategorized',
     'website': 'https://www.odoo.com/page/billing',
     # 'images' : ['images/accounts.jpeg','images/bank_statement.jpeg','images/cash_register.jpeg','images/chart_of_accounts.jpeg','images/customer_invoice.jpeg','images/journal_entries.jpeg'],
-    'depends': ['mail','portal','utm'],
+    'depends': [],
     'data': [
         #'views/rapport_views.xml',
         #'impression_repport/rapport.xml',
